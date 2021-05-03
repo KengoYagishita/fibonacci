c8ce545209985d9d35808957f6826b55630ebf0d
Kengo Yagishita <yagishitakengo1002@gmail.com>
2021-05-03 22:13:36 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict';
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    } else if(n === 1) {
+        return 1;
+    } 
+    // 再帰でfib関数を呼び出す
+    return fib(n - 1) + fib(n - 2);
+}
+
+// フィボナッチ数列を40番目まで出力する
+const length = 40;
+for (let i = 0; i <= length; i++){
+    console.log(fib(i));
+}
