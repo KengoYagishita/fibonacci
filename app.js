91a3cb7884ae64e0cdc1dcc4fb26ddc60bf29396
Kengo Yagishita <yagishitakengo1002@gmail.com>
2021-05-03 22:23:18 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 'use strict';
+// 連想配列Mapを利用して計算量を削減
+// 一度計算した結果を、連想配列に保存する方法を「メモ化」という
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if(n === 1) {
-        return 1;
+    // memoにすでにnに対応する解があった場合、その値を返す
+    if (memo.has(n)) {
+        return memo.get(n);
     } 
-    // 再帰でfib関数を呼び出す
-    return fib(n - 1) + fib(n - 2);
+    // memoに解がない場合、再帰でfib関数を呼び出して得た解をmemoに加える
+    let value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
 }
 
 // フィボナッチ数列を40番目まで出力する
